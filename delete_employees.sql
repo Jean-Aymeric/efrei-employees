95875dd1d44741d13aba437b4acefb28990a7da1
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2024-12-20 16:11:19 +0100

    update stored procedure delete_employees.sql

--- delete_employees.sql
+++ delete_employees.sql
@@ -4,6 +4,21 @@ DELIMITER //
 
 CREATE PROCEDURE deleteEmployee(IN _emp_no INT)
 BEGIN
+    DECLARE exist INT;
+    DECLARE errorMessage VARCHAR(255);
+
+    SET exist = (SELECT COUNT(*) FROM employees WHERE employees.emp_no = _emp_no);
+    IF exist = 0 THEN
+        SET errorMessage = CONCAT('Employee with emp_no = ', _emp_no, ' does not exist.');
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = errorMessage;
+    END IF;
+
+    SET exist = (SELECT COUNT(*) FROM salaries WHERE salaries.emp_no = _emp_no AND salaries.to_date = '9999-01-01');
+    IF exist = 0 THEN
+        SET errorMessage = CONCAT('Employee with emp_no = ', _emp_no, ' is already deleted.');
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = errorMessage;
+    END IF;
+
     UPDATE salaries SET to_date = CURDATE()
     WHERE emp_no = _emp_no AND to_date = '9999-01-01';
 
